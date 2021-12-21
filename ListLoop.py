b0f0aa769035605a932d6f6bba61188c503cc2b0
Promise <65677543+pro-pro95@users.noreply.github.com>
2021-12-21 07:43:03 -0600

    Update and rename Largest.py to ListLoop.py

--- ListLoop.py
+++ ListLoop.py
@@ -1,3 +1,5 @@
+# lists plus use of the for loop and conditional
+
 highest_building_so_far = 0
 print ("The Tallest building so far is:", highest_building_so_far)
 for building_height in [49, 58, 25, 69,35,78,51,78,78,32,90,500, 22, 5000,45]:
